978a61511ad57932c8738c6c57c044d3bc845c49
xiwc <xiwc87@yeah.net>
2015-04-10 11:38:35 +0800

    add `ui rail` & `ui sticky` & `ui menu`

--- custom.js
+++ custom.js
@@ -1,3 +1,17 @@
 jQuery(document).ready(function($) {
-	$(".ts-left-sidebar").sidebar().sidebar("attach events", ".ts-sidebar-trigger", "trigger");
+
+	$(".ts-left-sidebar").sidebar();
+
+	$(".ts-left-sidebar").sidebar("attach events", ".ts-sidebar-trigger", "trigger");
+
+	// $(".ts-left-sidebar").sidebar('setting', 'transition', 'overlay').sidebar("attach events", ".ts-sidebar-trigger", "trigger");
+
+	$('.ui.sticky').sticky({
+	    context: '.ts-main-container',
+	    offset: 84,
+	    pushing: true
+	  });
+
+	$('.ui.accordion').accordion();
+
 });
